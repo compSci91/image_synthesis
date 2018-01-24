a7ece4b19a0379f5b82656276f4678a272182cae
JoshuaHowell <howjosh@gmail.com>
2018-01-24 13:47:26 -0600

    Initial Commit

---
+++ BaseOpenGL/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BaseOpenGL
+//
+//  Created by Joshua Howell on 1/24/18.
+//  Copyright © 2018 Joshua Howell. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
